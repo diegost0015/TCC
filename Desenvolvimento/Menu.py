6f711836179be3c85c5d1681de04493e09f19932
diegost0015 <53863355+diegost0015@users.noreply.github.com>
2020-10-11 13:08:39 -0300

    Add files via upload
    Use o arquivo menu.py para rodar e não o mdi

--- Desenvolvimento/Menu.py
+++ Desenvolvimento/Menu.py
@@ -1,4 +1,6 @@
-from PyQt5 import uic, QtWidgets
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea, QAction, QMdiSubWindow, QTableView, QTableWidget, QPushButton
+from PyQt5 import uic
 import mysql.connector
 
 banco = mysql.connector.connect(
@@ -9,11 +11,14 @@ banco = mysql.connector.connect(
 
 )
 
+
+
 def chama_tela_cadastro(self):
     tela_cadastro.show()
 
 def chama_tela_estoque():
     tela_estoque.show()
+    lista = [("Editar"),("Deletar")]
 
     cursor = banco.cursor()
     comando_SQL = "SELECT *FROM  produtos"
@@ -22,11 +27,12 @@ def chama_tela_estoque():
     print(dados)
 
     tela_estoque.tableWidget.setRowCount(len(dados))
-    tela_estoque.tableWidget.setColumnCount(5)
+    tela_estoque.tableWidget.setColumnCount(10)
 
     for i in range(0, len(dados)):
-        for j in range(0, 5):
+        for j in range(0, 8):
             tela_estoque.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados[i][j])))
+            
 
 def cadastro():
     linha1 = tela_cadastro.lineEdit_2.text()
@@ -65,16 +71,45 @@ def deletar():
 
     tela_estoque.lineEdit.setText("")
 
+def Caixa():
+    tela_caixa.show()
+
+    linha = tela_caixa.lineEdit.text()
+
+    cursor = banco.cursor()
+    comando_SQL = "SELECT Nome, EAN, Preço FROM produtos WHERE EAN = (%s)"
+    dados = (str(linha))
+    cursor.execute(comando_SQL, (dados,))
+    dados = cursor.fetchall()
+    
+
+    print(dados)
+    
+
+    tela_caixa.tableWidget.setRowCount(len(dados))
+    tela_caixa.tableWidget.setColumnCount(3)
+
+    for i in range(0, len(dados)):
+        for j in range(0, 3):
+            tela_caixa.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados[i][j])))
+            
+
 
 app = QtWidgets.QApplication([])
 menu = uic.loadUi("Menu.ui")
+tela_caixa = uic.loadUi("Caixa.ui")
 tela_cadastro = uic.loadUi("Cadastro_Produto.ui")
 tela_estoque = uic.loadUi("Estoque.ui")
 tela_deletarProduto = uic.loadUi("Deletar_Produto.ui")
+
 tela_cadastro.pushButton.clicked.connect(cadastro)
 tela_deletarProduto.pushButton.clicked.connect(deletar)
-menu.actionCadastro.triggered.connect(chama_tela_cadastro)
+tela_caixa.pushButton.clicked.connect(Caixa)
+
+menu.actionProduto.triggered.connect(chama_tela_cadastro)
 menu.actionEstoque.triggered.connect(chama_tela_estoque)
+menu.actionCaixa.triggered.connect(Caixa)
+
 
 menu.show()
 app.exec()
--- Desenvolvimento/MenuMDI.py
+++ Desenvolvimento/MenuMDI.py
@@ -1,15 +1,12 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea, QAction, QMdiSubWindow, QTableWidget 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea, QAction, QMdiSubWindow, QTableWidget, QPushButton
 import sys
 from PyQt5 import uic
 import mysql.connector
 
 app = QApplication(sys.argv)
-menu = uic.loadUi("Menu.ui")
-tela_cadastro = uic.loadUi("Cadastro_Produto.ui")
-tela_deletarProduto = uic.loadUi("Deletar_Produto.ui")
-tela_estoque = uic.loadUi("Estoque.ui")
-tela_estoque_editar = uic.loadUi("Estoque_Editar.ui")
+
+
 
 banco = mysql.connector.connect(
     host = "localhost",
@@ -19,6 +16,12 @@ banco = mysql.connector.connect(
 
 )
 
+
+menu = uic.loadUi("Menu.ui")
+tela_cadastro = uic.loadUi("Cadastro_Produto.ui")
+tela_deletarProduto = uic.loadUi("Deletar_Produto.ui")
+tela_estoque = uic.loadUi("Estoque.ui")
+tela_estoque_editar = uic.loadUi("Estoque_Editar.ui")
 
 class MDIWindow(QMainWindow):
     
@@ -37,21 +40,27 @@ class MDIWindow(QMainWindow):
 
 
         file = bar.addMenu("Produtos")
-        file.addAction("Cadastro")
+        file.addAction("Cadastrar Produto")
         file.addAction("Deletar Produto")
         file.addAction("Estoque")
         file.addAction("Editar Estoque")
-        file.triggered[QAction].connect(self.WindowTrig)
+        file.triggered[QAction].connect(self.CriacaoTela)
         self.setWindowTitle("Papelaria ABC")
  
-    def WindowTrig(self, p):
-        if p.text() == "Cadastro":
+    def CriacaoTela(self, p):
+        
+        if p.text() == "Cadastrar Produto":
+            MDIWindow.count = MDIWindow.count + 1
             sub = QMdiSubWindow()
-            sub.setWidget(tela_cadastro)
-            sub.setWindowTitle("Cadastro Produto")
+            sub.setWindowTitle("Cadastrar Produto")
             sub.setGeometry(0,0,600,600)
             self.mdi.addSubWindow(sub)
+            sub.setWidget(tela_cadastro)
             sub.show()
+            
+                
+        
+            
 
         if p.text() == "Deletar Produto":
             sub = QMdiSubWindow()
@@ -91,13 +100,14 @@ def chama_tela_estoque():
     comando_SQL = "SELECT *FROM  Produtos"
     cursor.execute(comando_SQL)
     dados = cursor.fetchall()
-    print(dados)
+    
 
     tela_estoque.tableWidget.setRowCount(len(dados))
-    tela_estoque.tableWidget.setColumnCount(9)
+    tela_estoque.tableWidget.setColumnCount(8)
+
 
     for i in range(0, len(dados)):
-        for j in range(0, 9):
+        for j in range(0, 8):
             tela_estoque.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados[i][j])))
 
 def cadastro():
@@ -108,13 +118,12 @@ def cadastro():
     linha4 = tela_cadastro.lineEdit_Preco.text()
     linha5 = tela_cadastro.lineEdit_Altura.text()
     linha6 = tela_cadastro.lineEdit_Largura.text()
-    linha7 = tela_cadastro.lineEdit_Comprimento.text()
     linha8 = tela_cadastro.lineEdit_Descricao.text()
 
 
     cursor = banco.cursor()
-    comando_SQL = "INSERT INTO Produtos (Nome,EAN,Estoque,Preço,Altura,Largura,Comprimento,Descrição) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-    dados = (str(linha1), str(linha2), str(linha3), str(linha4), str(linha5), str(linha6), str(linha7), str(linha8))
+    comando_SQL = "INSERT INTO Produtos (Nome,EAN,Estoque,Preço,Altura,Largura,Descrição) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+    dados = (str(linha1), str(linha2), str(linha3), str(linha4), str(linha5), str(linha6),  str(linha8))
     cursor.execute(comando_SQL, dados)
     banco.commit()
 
@@ -125,7 +134,6 @@ def cadastro():
     tela_cadastro.lineEdit_Preco.setText("")
     tela_cadastro.lineEdit_Altura.setText("")
     tela_cadastro.lineEdit_Largura.setText("")
-    tela_cadastro.lineEdit_Comprimento.setText("")
     tela_cadastro.lineEdit_Descricao.setText("")
 
 
@@ -147,10 +155,13 @@ def deletar():
     
 
     tela_deletarProduto.tableWidget.setRowCount(len(dados2))
-    tela_deletarProduto.tableWidget.setColumnCount(9)
+    tela_deletarProduto.tableWidget.setColumnCount(8)
 
     for i in range(0, len(dados2)):
-        for j in range(0, 9):
+        btn_editar = QPushButton("Editar")
+        btn_editar.clicked.connect()
+        tela_deletarProduto.tableWidget.setItem(btn_editar)
+        for j in range(0, 8):
             tela_deletarProduto.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados2[i][j])))
 
     banco.commit()
